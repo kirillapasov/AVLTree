0a105d4ec9acff44729b36d98c60bbe9a0116a30
Kirill Apasov <kirill.apasov@yandex.ru>
2023-10-14 14:48:11 +0300

    Финальная версия 2

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -2,6 +2,7 @@ import java.util.Iterator;
 import java.util.Stack;
 /** BinaryTree - Бинарное дерево с итератором для обхода "in-order"
  BinaryTree представляет собой простую реализацию бинарного дерева на языке Java. Этот класс позволяет добавлять и удалять узлы, а также проверять наличие узла в дереве.
+ * @author Apasov, Zhuravlev
  Структура класса
  • Node: Представляет узел в бинарном дереве.
  • BinaryTree: Основной класс, реализующий структуру и операции с бинарным деревом.
@@ -11,13 +12,25 @@ import java.util.Stack;
  • TreeIterator: Вложенный класс, реализующий итератор для обхода в порядке "in-order".
  Итератор для обхода в порядке "in-order"
  Класс TreeIterator является вложенным классом внутри BinaryTree и реализует интерфейс Iterator<Integer> для обхода в порядке "in-order". Он позволяет итерироваться по бинарному дереву в следующем порядке: левый потомок, корень, правый потомок.
- Использование
+ Также создан вложенный класс Node, который представляет собой узел с полями ключя и ссылок на правый и левый узел. В классе создан метод Main, для примера использования BST.
+ Использование:
+
  Создайте экземпляр BinaryTree и добавьте узлы с помощью метода add(int value).
  Создайте экземпляр TreeIterator для обхода дерева в порядке "in-order".
  Используйте hasNext(), чтобы проверить наличие следующего узла для обхода.
  Используйте next(), чтобы получить следующий узел при обходе.
  */
 public class BinaryTree  {
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+        public Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
     Node root;
     private Node addRecursive(Node current, int value){
         if (current == null){
@@ -93,7 +106,7 @@ public class BinaryTree  {
     }
     class TreeIterator implements Iterator<Integer> {
         private Node current;
-        private Stack<Node> stack;
+        private final Stack<Node> stack;
 
         public TreeIterator() {
             stack = new Stack<>();
@@ -116,6 +129,24 @@ public class BinaryTree  {
             current = node.right;
             return node.value;
         }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        tree.add(8);
+        tree.add(1);
+        tree.add(2);
+        tree.delete(7);
+        tree.containsNode(7);
+        System.out.println(tree.containsNode(7));
+        BinaryTree.TreeIterator iterator = tree.new TreeIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+
     }
 
 
